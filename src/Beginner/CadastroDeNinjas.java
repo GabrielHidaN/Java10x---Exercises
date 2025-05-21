abc4ae06d6c82e565b643c35eb457fd197ec4c39
GabrielHidaN <victorbielgv7@gmail.com>
2025-05-21 18:48:12 -0300

    cadastro de ninjas

---
+++ src/Beginner/CadastroDeNinjas.java
@@ -0,0 +1,67 @@
+package Beginner;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class CadastroDeNinjas {
+    public static void main(String[] args) {
+
+        // input
+        Scanner sc = new Scanner(System.in);
+
+        // array
+        int maxNinjas = 3;
+        String [] ninjas = new String[maxNinjas];
+
+        // contadores
+        int quantityOfNinja = 0;
+        int option = 0 ;
+
+        String menu = """
+                ===== Cadastro de Ninjas =====
+                1. Cadastrar Ninja
+                2. Listar Ninjas
+                3. Sair
+                =>""";
+
+        while (option != 3){
+            System.out.print(menu);
+
+
+            try {
+                option = sc.nextInt();
+            } catch (InputMismatchException e){
+                sc.nextLine(); // Limpa o buffer do scanner
+                option = 0;
+            }
+
+            switch (option){
+                case 1:
+                    if (quantityOfNinja == maxNinjas){
+                        System.out.println("@@@ Número Máximo de Ninjas Alcançado! @@@");
+                    }
+                    else {
+                        System.out.print("Digite o Nome do Ninja Que deseja Adicionar\n=>");
+                        String ninja = sc.next();
+                        ninjas[quantityOfNinja] = ninja;
+                        quantityOfNinja ++;
+                    }
+                    break;
+                case 2:
+                    if (quantityOfNinja == 0){
+                        System.out.println("@@@ Nenhum Ninja Encontrado! @@@");
+                    }
+                    else {
+                        for (int i = 0; i < quantityOfNinja; i++) {
+                            System.out.println(ninjas[i]);
+                        }
+                    }
+                    break;
+                default:
+                    System.out.println("@@@ Opção Inválida! @@@");
+                    break;
+            }
+        }
+        sc.close();
+    }
+}
